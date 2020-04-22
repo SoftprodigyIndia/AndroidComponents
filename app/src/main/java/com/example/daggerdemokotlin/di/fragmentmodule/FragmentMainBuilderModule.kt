39cc4ca36a9efd04b41fcc0d014f16e43188021c
shamsher <sam.pathania1992.sp@gmail.com>
2020-04-22 19:32:35 +0530

    Bottom navigation bar in dagger 2

--- app/src/main/java/com/example/daggerdemokotlin/di/fragmentmodule/FragmentMainBuilderModule.kt
+++ app/src/main/java/com/example/daggerdemokotlin/di/fragmentmodule/FragmentMainBuilderModule.kt
@@ -1,20 +1,39 @@
 package com.example.daggerdemokotlin.di.fragmentmodule
 
+import android.provider.ContactsContract
 import com.example.daggerdemokotlin.ui.postfragment.PostAuthModel
 import com.example.daggerdemokotlin.ui.postfragment.PostFragment
 import com.example.daggerdemokotlin.ui.postfragment.PostViewModel
 import com.example.daggerdemokotlin.ui.profile.ProfileAuthModel
 import com.example.daggerdemokotlin.ui.profile.ProfileFragment
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class FragmentMainBuilderModule {
 
     @ContributesAndroidInjector(modules = arrayOf(ProfileAuthModel::class))
-    abstract fun contributeProfileFragment(): ProfileFragment
+        abstract fun contributeProfileFragment(): ProfileFragment
 
     @ContributesAndroidInjector(modules = arrayOf(PostAuthModel::class))
     abstract fun contributePostFragment(): PostFragment
+
+    @Module
+    companion object{
+
+        @Provides
+        @JvmStatic
+        fun profileFragment():ProfileFragment{
+            return  ProfileFragment()
+        }
+
+        @Provides
+        @JvmStatic
+        fun  homeFragment():PostFragment{
+            return  PostFragment()
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/daggerdemokotlin/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/daggerdemokotlin/ui/home/HomeActivity.kt
@@ -4,18 +4,60 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.daggerdemokotlin.R
 import com.example.daggerdemokotlin.ui.postfragment.PostFragment
+import com.example.daggerdemokotlin.ui.profile.ProfileFragment
 import dagger.android.support.DaggerAppCompatActivity
+import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.activity_home.*
+import nl.joery.animatedbottombar.AnimatedBottomBar
+import javax.inject.Inject
 
 class HomeActivity : DaggerAppCompatActivity() {
+    @Inject
+    lateinit var homeFragment:PostFragment
 
+    @Inject
+    lateinit var profileFragment: ProfileFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        loadFragment()
+        setBottom()
     }
 
-    private fun loadFragment() {
-        supportFragmentManager.beginTransaction().
-            replace(R.id.frame, PostFragment()).commit()
+    private fun setBottom() {
+        bottom_bar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener{
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+
+                selectTab(newIndex)
+            }
+
+        })
+    }
+
+    private fun selectTab(newIndex: Int) {
+        when(newIndex){
+            0->{
+                loadFragment(homeFragment)
+            }
+            2->{
+                loadFragment(profileFragment)
+            }
+
+
+        }
+
+
+    }
+    private fun loadFragment(frag: DaggerFragment) {
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame, frag)
+            .commit()
+
     }
 }
